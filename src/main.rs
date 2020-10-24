1c9ce97ca34fe4a9157c73ffc4797032dc5957e8
xvi <jrip03@gmail.com>
2020-10-24 01:54:02 -0400

    finished building verion

--- src/main.rs
+++ src/main.rs
@@ -19,4 +19,34 @@ struct PostJsonObj {
 	site: String
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Handlers
 
+async fn json_handler(
+	req_obj: web::Json<PostJsonObj>,
+	_req: HttpRequest,
+) -> HttpResponse {
+	HttpResponse::Ok().json(req_obj.0)
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Main
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+	std::env::set_var("RUST_LOG", "actix_web=info");
+	env_logger::init();
+
+	HttpServer::new(|| {
+		App::new()
+			.wrap(middleware::Logger::default())
+			.service(web::resource("/ticket")
+				.data(web::JsonConfig::default().limit(512))
+				.route(web::post().to(json_handler))
+			)
+	})
+	.bind("127.0.0.1:8000")?
+	.run()
+	.await
+}
